1805fe9e121413bc78d354b2ffb6b4b7dbac5727
Dessaigne Julien <julien.dessaigne@etu.u-bordeaux.fr>
2023-06-28 16:51:36 +0200

    Premier poc de visualisation 3D

---
+++ src/Scene.js
@@ -0,0 +1,69 @@
+import * as THREE from "three";
+import imageDeplacement from "./deplacement.svg";
+import imageTooltip from "./infobulle.svg";
+
+class Scene {
+    constructor(image, scene) {
+        this.image = image
+        this.points = []
+        this.sprites = []
+        this.scene = scene
+    }
+
+    createScene() {
+        const geometry = new THREE.SphereGeometry(50, 32, 16);
+        const texture = new THREE.TextureLoader().load(this.image);
+        texture.wrapS = THREE.RepeatWrapping
+        texture.repeat.x = -1
+        const material = new THREE.MeshBasicMaterial({
+            map: texture,
+            side: THREE.DoubleSide,
+        });
+        this.sphere = new THREE.Mesh(geometry, material);
+        this.scene.add(this.sphere)
+        this.points.forEach(function(point){
+            this.addTooltip(point)
+        }.bind(this))
+    }
+
+    addPoint(point){
+        this.points.push(point)
+    }
+
+    addTooltip(point) {
+        console.log(point)
+        let image = "";
+        switch (point.typeSprite) {
+            case "deplacement":
+                image = imageDeplacement
+                break;
+            case "tooltip":
+                image = imageTooltip
+                break;
+        }
+        let map = new THREE.TextureLoader().load(image);
+        let materialSprite = new THREE.SpriteMaterial({map: map});
+        let sprite = new THREE.Sprite(materialSprite);
+        sprite.name = point.nom
+
+        sprite.typeSprite = point.typeSprite
+        sprite.position.copy(point.position.clone().normalize().multiplyScalar(30))
+        sprite.scale.multiplyScalar(3)
+        console.log(sprite)
+        this.scene.add(sprite);
+        this.sprites.push(sprite)
+        sprite.onClick = () => {
+            this.destroy()
+            point.scene.createScene(this.scene)
+        }
+    }
+
+    destroy(){
+        this.scene.remove(this.sphere)
+        this.sprites.forEach((sprite)=>{
+            this.scene.remove(sprite)
+        })
+    }
+}
+
+export default Scene
