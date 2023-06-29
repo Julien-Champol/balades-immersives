1a19598bc46bd6c991f5b8642fb8c2b2d24dbede
Dessaigne Julien <julien.dessaigne@etu.u-bordeaux.fr>
2023-06-29 22:40:01 +0200

    Correction merge

--- src/Scene360.js
+++ src/Scene360.js
@@ -1,10 +1,12 @@
 import React, {useEffect, useRef} from 'react';
 import * as THREE from 'three';
-import {OrbitControls} from "three/addons/controls/OrbitControls";
 import image360 from './Images/PXL_20230628_075033818.PHOTOSPHERE.jpg'
 import image360_2 from './Images/PXL_20230628_133755950.PHOTOSPHERE~2.jpg';
 import {Vector3} from "three";
-import Scene from './Model/Scene'
+import Scene from './Model/Scene';
+import Camera from './Model/Camera';
+import Renderer from './Model/Renderer';
+import Controls from './Model/Controls';
 import PointDeplacement from "./Model/PointDeplacement";
 import PointInteret from "./Model/PointInteret";
 function Scene360() {
@@ -14,6 +16,7 @@ function Scene360() {
     const rendererRef = useRef();
     const containerRef = useRef()
     const canvasRef = useRef();
+    const controlerRef = useRef();
 
     let rayCaster = new THREE.Raycaster()
     const tooltip = document.querySelector('.tooltip')
@@ -22,26 +25,18 @@ function Scene360() {
         const container = document.body
         containerRef.current = container
 
-        const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 200);
-        cameraRef.current = camera;
-
-        const renderer = new THREE.WebGLRenderer({canvas: canvasRef.current});
-        rendererRef.current = renderer;
-
-        const controls = new OrbitControls(camera, renderer.domElement);
-        controls.rotateSpeed = -0.5
-        controls.enableZoom = false;
-        camera.position.set(-1, 0, 0);
-        controls.update();
-
-        console.log(controls)
-
         // Scene et Controle
         const scene = new THREE.Scene();
 
         let oScene1 = new Scene(image360, scene)
 
         let oScene2 = new Scene(image360_2, scene)
+
+        let oCamera = new Camera(-1,0,0);
+
+        let oRenderer = new Renderer();
+        oCamera.createCamera();
+        oRenderer.createRenderer();
 
         let oPointDeplacement1 = new PointDeplacement(new Vector3(-17.60, -19.48, 42.50), "Déplacement", oScene2);
         let oPointDeplacement2 = new PointDeplacement(new Vector3(42.54621820468249,-25.619583468013133,1.8401114612655096), "Déplacement", oScene1)
@@ -52,18 +47,25 @@ function Scene360() {
         oScene2.addPoint(oPointDeplacement2)
         oScene1.createScene()
 
-        sceneRef_2.current = oScene2
-        sceneRef.current = oScene1
+        sceneRef_2.current = oScene2;
+        sceneRef.current = oScene1;
+        cameraRef.current = oCamera;
+        rendererRef.current = oRenderer;
+
+
+        let oControl = new Controls(cameraRef.current.camera,rendererRef.current.renderer);
+
+        oControl.createControls();
+        controlerRef.current = oControl;
+
+
+        controlerRef.current.controls.update();
+
 
 
         // Render
-        renderer.setSize(window.innerWidth, window.innerHeight);
-        container.appendChild(renderer.domElement);
 
-        // Tooltip
-        // oScene1.addTooltip(new Vector3(30, 0, 0), 'Tableau', "tooltip")
-        // oScene1.addTooltip(new Vector3(-17.60, -19.48, 42.50), 'Déplacement', "deplacement")
-
+        container.appendChild(rendererRef.current.renderer.domElement);
 
         animate();
 
@@ -71,7 +73,7 @@ function Scene360() {
         container.addEventListener('click', onClick)
         container.addEventListener('mousemove', onMouseMove)
         return () => {
-            renderer.dispose();
+            rendererRef.current.renderer.dispose();
         };
 
     }, []);
@@ -80,7 +82,7 @@ function Scene360() {
     const animate = () => {
         requestAnimationFrame(animate);
 
-        rendererRef.current.render(sceneRef.current.scene, cameraRef.current);
+        rendererRef.current.renderer.render(sceneRef.current.scene, cameraRef.current.camera);
     };
 
     function onMouseMove(e) {
@@ -88,12 +90,12 @@ function Scene360() {
             (e.clientX / window.innerWidth) * 2 - 1,
             -(e.clientY / window.innerHeight) * 2 + 1
         )
-        rayCaster.setFromCamera(mouse, cameraRef.current)
+        rayCaster.setFromCamera(mouse, cameraRef.current.camera)
         let foundSprite = false;
         let intersects = rayCaster.intersectObjects(sceneRef.current.scene.children);
         intersects.forEach(function (intersect) {
             if (intersect.object.type === 'Sprite') {
-                let p = intersect.object.position.clone().project(cameraRef.current)
+                let p = intersect.object.position.clone().project(cameraRef.current.camera)
                 tooltip.style.top = (-1 * p.y + 1) * window.innerHeight / 2 - 50 + 'px'
                 tooltip.style.left = (p.x + 1) * window.innerWidth / 2 + 'px'
                 tooltip.classList.add('is-active')
@@ -115,7 +117,7 @@ function Scene360() {
         )
 
         // let rayCaster = new THREE.Raycaster()
-        rayCaster.setFromCamera(mouse, cameraRef.current)
+        rayCaster.setFromCamera(mouse, cameraRef.current.camera)
 
         let intersects = rayCaster.intersectObjects(sceneRef.current.scene.children);
         //
@@ -137,8 +139,8 @@ function Scene360() {
 
 
     const onResize = () => {
-        rendererRef.current.setSize(window.innerWidth, window.innerHeight)
-        cameraRef.current.aspect = window.innerWidth / window.innerHeight
+        rendererRef.current.renderer.setSize(window.innerWidth, window.innerHeight)
+        cameraRef.current.camera.aspect = window.innerWidth / window.innerHeight
     }
 
     return <canvas ref={canvasRef}/>;
