e82a353844809754afcb604e5f37a04843eb31aa
jchampol <jchampol@etu.u-bordeaux.fr>
2023-07-06 10:55:34 +0200

    Changement de marker au zoom

--- src/Components/Markers.js
+++ src/Components/Markers.js
@@ -1,12 +1,14 @@
 import L from 'leaflet';
 import { Marker, Popup } from 'react-leaflet';
-import iconLocation from "../Images/location.png";
-
 import { Link } from "react-router-dom";
+import iconLocation from "../Images/location.png";
+import zoomedIconLocation from "../Images/zoomedLocation.png";
 
 const Markers = (props) => {
 
     const batiments = props.buildings;
+
+    const zoomedBuilding = props.zoomedBuilding;
 
     const iconMarker = new L.Icon({
         iconUrl: iconLocation,
@@ -14,13 +16,19 @@ const Markers = (props) => {
         className: 'leaflet-div-icon'
     });
 
+    const zoomedIconMarker = new L.Icon({
+        iconUrl: zoomedIconLocation,
+        iconSize: new L.Point(30, 30),
+        className: 'leaflet-div-icon'
+    })
+
     return (
         <>
             {batiments.map((batiment) => (
                 <Marker
                     key={batiment._id}
                     position={[batiment.latitude, batiment.longitude]}
-                    icon={iconMarker}
+                    icon={batiment === zoomedBuilding ? zoomedIconMarker : iconMarker}
                 >
                     <Popup>
                         <div className="popupCarte">
@@ -30,7 +38,7 @@ const Markers = (props) => {
                             <Link className="btn btn-success" id="boutonRentrerBatiment" to={`/scene360/${batiment._id}`}>Rentrer dans le
                                 bâtiment</Link>
                             {batiment.url.length > 0 && <a className="linkPopup" href={batiment.url} target="_blank" rel="noreferrer">
-                                consulter le site
+                                Consulter le site
                             </a>}
                         </div>
 
